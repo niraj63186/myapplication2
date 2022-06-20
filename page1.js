7569f0d7756da4ab4c374892c9ba20cf09f77cce
D5_63186_Niraj <nirajshinde1997@gmail.com>
2022-06-20 16:42:18 +0530

    page1 add function

--- page1.js
+++ page1.js
@@ -1,1 +1,9 @@
-console.log('hello cdac')
+function isEven(number){
+    if(number%2==0)
+        console.log('number is even')
+    else
+        console.log('not even')
+}
+
+isEven(10)
+isEven(11)
